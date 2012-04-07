fbb808c70eb5599700fe02c6ff9e4970269f51e3
Piotr Barczuk <piotr.barczuk@one99.pl>
2012-04-07 02:39:31 +0200

    added readme

--- config/routes.rb
+++ config/routes.rb
@@ -49,7 +49,6 @@ Bowling::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
   root :to => 'bowling#go'
-
   # See how all your routes lay out with "rake routes"
 
   # This is a legacy wild controller route that's not recommended for RESTful applications.
